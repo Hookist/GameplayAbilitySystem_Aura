bb8d0ce49ca6e807e2342a6c82c981ec064c2719
Stas Bakalo <spartanstas@gmail.com>
2023-11-16 20:38:43 +0200

    86. Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Copyright Stas Company
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attribute.Secondary.Armor"),
+		FString("Reduces damage taken, improves Block Chance"));
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,27 @@
+// Copyright Stas Company
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/*
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+
+	static const FAuraGameplayTags Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+
+protected:
+
+private:
+
+	static  FAuraGameplayTags GameplayTags;
+	
+};
