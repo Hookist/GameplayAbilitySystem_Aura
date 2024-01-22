f4c4099bd3a6a6d212b6b69e24694d3ae650d353
Stas Bakalo <spartanstas@gmail.com>
2024-01-22 22:03:13 +0200

    117. Send Mouse Cursor Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -2,8 +2,8 @@
 
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
-
 #include "AbilitySystemComponent.h"
+
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -13,13 +13,16 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
-	FVector hitLocation;
-
-	APlayerController* pawnPlayerController = Ability->GetActorInfo().PlayerController.Get();
-	FHitResult cursorHit;
-	pawnPlayerController->GetHitResultUnderCursor(ECC_Visibility, false, cursorHit);
-	hitLocation = cursorHit.Location;
-	ValidData.Broadcast(hitLocation);
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		//TODO: We are on the server, so listen for target data.
+	}
+	
 	// AActor* avatarActor = AbilitySystemComponent.Get()->GetAvatarActor();
 	// if (!avatarActor)
 	// 	return;
@@ -43,4 +46,32 @@ void UTargetDataUnderMouse::Activate()
 	// 	return;
 
 
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow scopedPrediction(AbilitySystemComponent.Get());
+	
+	FVector hitLocation;
+	APlayerController* pawnPlayerController = Ability->GetActorInfo().PlayerController.Get();
+	FHitResult cursorHit;
+	pawnPlayerController->GetHitResultUnderCursor(ECC_Visibility, false, cursorHit);
+	hitLocation = cursorHit.Location;
+	
+	FGameplayAbilityTargetDataHandle dataHandle;	
+	FGameplayAbilityTargetData_SingleTargetHit* data = new FGameplayAbilityTargetData_SingleTargetHit();
+	data->HitResult = cursorHit;
+	dataHandle.Add(data);
+	
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		data,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(dataHandle);
+	}
 }
