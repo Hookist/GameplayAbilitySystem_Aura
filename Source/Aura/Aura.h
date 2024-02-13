71875cb7ebae28d4c8122bb880cd2c85a6c938f5
Stas Bakalo <spartanstas@gmail.com>
2024-02-13 13:55:59 +0200

    123. Projectile Collision Channel

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,9 +5,10 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 
 /* TODO: less view range effect
- * no ear effect or no noise
+ * no hear effect or no noise
  * contusion that mess movement
  * slow movement (maybe make mode small)
  * make model bigger (maybe increase damage and movespeed)
--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -4,6 +4,7 @@
 #include "Actor/AuraProjectile.h"
 
 #include "NiagaraFunctionLibrary.h"
+#include "Aura/Aura.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
@@ -19,6 +20,7 @@ AAuraProjectile::AAuraProjectile()
 
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere);
+	Sphere->SetCollisionObjectType(ECC_Projectile);
 	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
 	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
@@ -42,7 +44,9 @@ void AAuraProjectile::BeginPlay()
 
 void AAuraProjectile::Destroyed()
 {
-	FlyingSoundComponent->Stop();
+	if (FlyingSoundComponent)
+		FlyingSoundComponent->Stop();
+	
 	if (!bHit && !HasAuthority())
 	{
 		UGameplayStatics::PlaySoundAtLocation(
