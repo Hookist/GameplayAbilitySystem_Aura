f211975d5b619b4c9df127836c019f8e0ea1ba5d
Stas Bakalo <spartanstas@gmail.com>
2024-02-28 15:50:24 +0200

    143. Damage Execution Calculation

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright Stas Company
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                              FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* sourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* targetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* sourceAvatar = sourceASC ? sourceASC->GetAvatarActor() : nullptr;
+	const AActor* targetAvatar = targetASC ? targetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& spec = ExecutionParams.GetOwningSpec();
+}
