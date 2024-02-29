888fee38e6e0f1cf3e83702df96fd2d0622cffc4
Stas Bakalo <spartanstas@gmail.com>
2024-02-29 19:44:54 +0200

    154. Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright Stas Company
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
