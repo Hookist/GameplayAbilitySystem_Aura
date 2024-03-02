32c0a9c256c446405505045898cb6d51e0da8c82
Stas Bakalo <spartanstas@gmail.com>
2024-03-02 23:19:36 +0200

    165. Behavior Tree Service

---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,12 @@
+// Copyright Stas Company
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, *AIOwner.GetName());
+	GEngine->AddOnScreenDebugMessage(2, 1.f, FColor::Green, *ActorOwner.GetName());
+}
