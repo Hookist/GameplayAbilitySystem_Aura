8e9a5d99f79912fa12c869c9543bcb64dfc454d5
Stas Bakalo <spartanstas@gmail.com>
2024-03-03 18:18:37 +0200

    170. Attack Behavior Tree Task

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,11 @@
+// Copyright Stas Company
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type result = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	return result;
+}
