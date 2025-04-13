27ff0271f064708ee1d4f2c3a0e40bffeee20c11
spartanstas <spartanstas@gmail.com>
2025-04-13 23:07:33 +0300

    250. Level Up Info Data Asset

---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInformation.h
@@ -0,0 +1,42 @@
+﻿// Copyright Stas Company
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInformation.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FLevelUpInformation
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 SpellPointAward = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FLevelUpInformation> LevelUpInformation;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	int32 FindLevelForXP(int32 InXP) const;
+};
