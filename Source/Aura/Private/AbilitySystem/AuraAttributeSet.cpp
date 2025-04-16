3916b1809572bc912f8250910b618bd29cf9c26b
spartanstas <spartanstas@gmail.com>
2025-04-17 02:32:47 +0300

    255. Incoming XP Meta Attribute

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -8,6 +8,7 @@
 #include "AuraGameplayTags.h"
 #include "GameplayEffectExtension.h"
 #include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Aura/AuraLogChannels.h"
 #include "GameFramework/Character.h"
 #include "Interaction/CombatInterface.h"
 #include "Kismet/GameplayStatics.h"
@@ -138,6 +139,13 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			const bool bCriticalHit = UAuraAbilitySystemLibrary::IsCriticalHit(props.EffectContextHandle);
 			ShowFloatingText(props, localIncomeDamage, bBlock, bCriticalHit);
 		}
+	}
+
+	if (Data.EvaluatedData.Attribute == GetIncomingXPAttribute())
+	{
+		const float localIncomingXP = GetIncomingXP();
+		SetIncomingXP(0.f);
+		UE_LOG(LogAura, Display, TEXT("New XP value = %f"), localIncomingXP);
 	}
 }
 
